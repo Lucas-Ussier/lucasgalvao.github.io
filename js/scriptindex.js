3822999c49bf0d8812e8c5359ca0d944e60963e9
Lucas <lukedbzgalvao@gmail.com>
2024-03-22 16:25:42 -0300

    Atualização do portifólio

---
+++ js/scriptindex.js
@@ -0,0 +1,4 @@
+function iframeVisible() {
+    var iframe = window.document.getElementById('tela');
+    iframe.innerHTML = `<iframe src="" id="frame" name="tela" frameborder="0" style="width: 95%; height: 400px; display: block; margin: auto; border-radius: 15px;"></iframe>`
+}
